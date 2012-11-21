c48d2b404a1260f3e44bb5b9c87b92e56b87c9ca
Ivo Vachkov <ivo.vachkov@gmail.com>
2012-11-21 17:52:17 +0200

    initial list of expected #include for the client

--- client-server-text-proto/client-text-proto.c
+++ client-server-text-proto/client-text-proto.c
@@ -1,10 +1,17 @@
+#include <sys/time.h>
 #include <sys/types.h>
-
+#include <sys/socket.h>
+#include <sys/select.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 
 #include <tprot.h>
 
-
+/* Test TPROT Client Application */
 int main(char argc, char **argv)
 {
 	
